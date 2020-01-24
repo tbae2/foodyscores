08ec0ebb4eff62643b1d0dae1c8607277841c0df
Thomas Bae <thomas.bae0@gmail.com>
2020-01-24 15:09:38 -0500

    adding config for holding city / code distctions, initpy for module importing. sys so the script can reference its own location properly

--- tasks/gahealth.py
+++ tasks/gahealth.py
@@ -2,7 +2,8 @@
 import requests
 import sys
 import json
-
+import datetime
+import os
 
 
 
@@ -27,31 +28,41 @@ class yelpInfo():
 
 def grabHealthScores():
     print('grabbing url')
+    # ga department of health api that stores all the health reports for food establishments 
     base_url = 'https://ga.healthinspections.us/stateofgeorgia/API/index.cfm/search/%7B%22city%22:%22TUFSSUVUVEE=%22,%22keyword%22:%22%22%7D/'
     print(base_url)
     pageCount = 0
-    maxPageCount = 3
+    currentDate = datetime.date.today()
+    # maxPageCount = 3
     healthScoreHolder = []
     noMoreData = False
-    storeFile = open("testfile.csv","wt")
-    print('test test test')
-
+    storeFile = open(f"testfile{currentDate}.csv","wt")
+    storePath = storeFile.name
+    print(os.path.realpath(storePath))
     # while True:
-    # while noMoreData == False:
-    while pageCount < maxPageCount:
-        healthReq = requests.get("%s%s" % (base_url,pageCount))
-        if len(healthReq.json()) > 0:
-            healthScoreHolder += healthReq.json()
-            pageCount += 1
-        else:
-            noMoreData = True
+    # while pageCount < maxPageCount:
+    # loop through requests for given city url until the response is blank meaning no more data to return
+    while noMoreData == False:
+        try:    
+            healthReq = requests.get("%s%s" % (base_url,pageCount))
+            if len(healthReq.json()) > 0:
+                healthScoreHolder += healthReq.json()
+                pageCount += 1
+                print(str(pageCount))
+            else:
+                noMoreData = True
+        except Exception as e:
+            print(healthReq.text)
+            print(healthReq.status_code)
+            raise e
         
         
-        print(str(healthReq.status_code))
-    print(str(healthScoreHolder))
+    # print(str(healthReq.status_code))
+    # print(str(healthScoreHolder))
     # storeFile.write(healthScoreHolder)
     json.dump(healthScoreHolder,storeFile)
     storeFile.close()
+    return storePath
     
 if __name__ == "__main__":
     grabHealthScores()
