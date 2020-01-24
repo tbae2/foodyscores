dd99757b1bcc19ca4a9eaa80440e146bfb7ecc1d
Thomas Bae <thomas.bae0@gmail.com>
2020-01-24 15:35:20 -0500

    passes for now to quiet the errors

--- tasks/yelpdata.py
+++ tasks/yelpdata.py
@@ -12,12 +12,13 @@ def matchScores():
     ## this should pull data from gahealth scores data
     ## call the yelp api to match the data by location
     ## store this data in a csv 
-
+    pass
 def cleanYelpOutput():
     ## this should format to desired input to table
     ## cleans attributes not needed, include attributes that would help match (FK)
-
+    pass
 
 def loadYelpOutput():
     ## load the processed data into the database for analytics usage
     ## further thought and research needed here
+    pass
